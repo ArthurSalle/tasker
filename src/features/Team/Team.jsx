05a5b1da7d88e4867282e089dde25ec9c0fd0a75
Arthur Sallé <arthur.salle95@gmail.com>
2024-05-31 16:45:14 +0200

    ✨ create app pages

---
+++ src/features/Team/Team.jsx
@@ -0,0 +1,11 @@
+import AttendeesSection from "./AttendeesSection"
+
+export default function Team() {
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-medium">Manage your team</h2>
+
+      <AttendeesSection />
+    </div>
+  )
+}
